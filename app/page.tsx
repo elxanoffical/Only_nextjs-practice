ae9319a9dda991086c15c121c7f6909020093837
Elxan <elxan.offical.07@gmail.com>
2025-01-30 19:27:25 +0400

    Link navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+
+import Link from "next/link"
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h2>Welcome to Home Page</h2>
+      <Link className="border-2 border-black" href='/blog'>Blog</Link>
+      <Link className="border-2 border-black" href='/products'>products</Link>
+    </div>
   )
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import Link from "next/link";
 
 export default function Products() {
   return (
     <div>
-        <h1>Products List</h1>
-        <h1>Products 1</h1>
-        <h1>Products 2</h1>
-        <h1>Products 3</h1>
+      <Link className="border-2 border-black" href="/">
+        HomePage
+      </Link>
+      <h1>Products List</h1>
+      <h1>
+        <Link className="border-b-2 border-red-600" href="products/1">
+          Products 1
+        </Link>
+      </h1>
+      <h1>
+        <Link className="border-b-2 border-red-600" href="products/2">
+          Products 2
+        </Link>
+      </h1>
+      <h1>
+        <Link className="border-b-2 border-red-600" href="products/3" replace> // replace yazdigda arxaya qayitmaqa click etdikde HomePage yonlendirir
+          Products 3
+        </Link>
+      </h1>
     </div>
-  )
+  );
 }
