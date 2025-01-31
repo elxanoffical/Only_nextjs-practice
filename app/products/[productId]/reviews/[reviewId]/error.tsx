650261182a2f09846169d980296f1fcd7893e8d5
Elxan <elxan.offical.07@gmail.com>
2025-01-31 23:48:54 +0400

    Recovering from Errors

--- app/products/[productId]/reviews/[reviewId]/error.tsx
+++ app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,11 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return <div>{error.message} <button className="px-4 py-2 border rounded-full border-black" onClick={reset}>try again</button></div>;
 }
